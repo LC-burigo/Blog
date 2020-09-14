f3ba52d2e04f219b0dc91ffac0d1c3076214aa51
LC-burigo <burigo-jm@hotmail.com>
2020-09-14 18:57:37 -0300

    terceiro commit

--- projeto/blog_app/views.py
+++ projeto/blog_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import (TemplatesView, ListView)
+from blog_app.models import Post, Comment
 
-# Create your views here.
+
+class AboutView(TemplatesView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
