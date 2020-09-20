f9a2350a287a4f8fb5879bc1a69a785b587c8529
LC-burigo <burigo-jm@hotmail.com>
2020-09-20 18:13:24 -0300

    eighth commit

--- projeto/blog_app/urls.py
+++ projeto/blog_app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from blog_app import views
 
+
 urlpatterns = [
     url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^about/$', views.AboutView.as_view(), name='about'),
